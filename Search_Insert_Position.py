ccb630d371d18260f4f4d4a0ac2f0190a3f06b72
CathalMcsweeney <58216678+CathalMcsweeney@users.noreply.github.com>
2024-07-11 16:01:39 +0100

    improved insert position algorithm

--- Search_Insert_Position.py
+++ Search_Insert_Position.py
@@ -17,9 +17,29 @@ class Solution(object):
 
         return ans
     
-solution = Solution()
+    def searchInsertFaster(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
 
+        left = 0
+        right = len(nums)
+
+        while left < right:
+            mid = (left + right) // 2
+            if target == nums[mid]:
+                return mid
+            if target < mid:
+                right = nums[mid]
+            else:
+                left = mid + 1
+
+        return left
+    
+solution = Solution()
 nums = [1,3,5,6]
 target = 7
 
-print(solution.searchInsert(nums, target))
+print(solution.searchInsertFaster(nums, target))
